adedc15d2182dd09e34999c6ce9c4a85c742542f
Ofish <vc.ofir@gmail.com>
2025-02-18 20:29:56 -0500

    jump code added

--- My project/Assets/Scripts/Player Movement.cs
+++ My project/Assets/Scripts/Player Movement.cs
@@ -6,35 +6,93 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Rigidbody2D rb;
+
     private float horizontalMove = 0f;
-    private float verticalMove = 0f;
     [SerializeField] private float speed = 10f;
+
+    [SerializeField] Transform groundCheck;
+    public LayerMask groundLayer;
+
     [SerializeField] private float jumpForce = 100f;
+    private bool verticalMove;
+    [SerializeField] private float fallMult;
+    private Vector2 vecGravity;
+
+    [SerializeField] private float jumpTime;
+    [SerializeField] private float jumpMult;
+    private bool isJumping;
+    private float jumpCounter;
+    private bool jumpReleased;
+
     void Start()
     {
+        vecGravity = new Vector2(0, -Physics2D.gravity.y);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isGrounded() && Input.GetButtonDown("Jump"))
+        {
+            verticalMove = true;
+        }
         horizontalMove = Input.GetAxisRaw("Horizontal");
-        verticalMove = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            jumpReleased = true;
+            isJumping = false;
+            jumpCounter = 0;
+        }
 
     }
 
     void FixedUpdate()
     {
-        transform.position += new Vector3(horizontalMove * speed, 0, 0);
+        rb.linearVelocity = new Vector2(horizontalMove * speed, rb.linearVelocity.y);
 
-        //if (verticalMove > 0 && Mathf.Abs(rigidbody2D.linearVelocityY) < .001f )
-        //{
-        //    rigidbody2D.linearVelocityY += verticalMove * jumpForce;
-        //}
-        //else
-        //{
-        //    rigidbody2D.linearVelocityY = 0;
-        //}
+        if (verticalMove)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isJumping = true;
+            jumpCounter = 0;
+            verticalMove = false;
+        }
 
+        if (rb.linearVelocity.y > 0 && isJumping)
+        {
+            jumpCounter += Time.deltaTime;
+            if (jumpCounter > jumpTime)
+            {
+                isJumping = false;
+            }
+            float t = jumpCounter / jumpTime;
+            float currentJumpM = jumpMult;
+
+            if (t > 0.5f)
+            {
+                currentJumpM = jumpMult*(1-t);
+            }
+
+            rb.linearVelocity += vecGravity * currentJumpM * Time.deltaTime;
+        }
+
+        if (jumpReleased && rb.linearVelocity.y > 0)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * .6f);
+        }
+
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity -= vecGravity * fallMult * Time.deltaTime;
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1.2f, .1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
     }
 
 }
